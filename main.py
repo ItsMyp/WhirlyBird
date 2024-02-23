f1c39eeb181737f3a7339a711e44edfa7f9adb14
ItsMyp <146660335+ItsMyp@users.noreply.github.com>
2024-02-23 20:00:34 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -39,10 +39,84 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
-player = Player('racket.png', 30, 200, 4, 50, 150) 
+Playerr = Player('racket.png', 30, 200, 4, 50, 150) 
 Platform = GameSprite('racket.png', 520, 200, 4, 50, 150)
-Spike = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+Spike = GameSprite('spikes.png', 200, 200, 4, 50, 50)
+
+mixer.init() 
+fire = mixer.Sound(".ogg")
+fire.set_volume(0.1)
 
 font.init()
 font = font.Font(None, 35)
-lose1 = font.render('YOU LOSE', True, (180, 0, 0))
+lose = font.render('YOU LOSE', True, (180, 0, 0))
+#the variable game is over 
+ 
+finish = False 
+ 
+ 
+#main game cycle
+run = True 
+
+while run: 
+ 
+    #the event of clicking on the close button 
+     
+    for e in event.get(): 
+        if e.type == QUIT: 
+            run = False 
+            #the event of clicking to space - sprite is shooting
+        elif e.type == KEYDOWN:
+            if e.key == K_LEFT:
+
+            elif e.key == K_RIGHT:
+                #M
+
+
+    if not finish: 
+ 
+        window.blit((0, 0)) 
+         
+        #writing text on screen 
+ 
+        lose = font.render("Рахунок:" + str(score), 1, (255, 255, 255)) 
+        window.blit(lose, (10, 20)) 
+ 
+        #sprites moves
+ 
+        Playerr.update() 
+        Platform.update() 
+        Spike.update()
+         
+        Platform.draw(window) 
+        Spike.draw(window)
+
+        if sprite.spritecollide(rocket, monsters, False):
+            finish = True
+            window.blit(lose, [200, 200])
+ 
+        collides = sprite.groupcollide(Platform, Spike, True, True)
+        for c in collides:
+
+            score += 1
+
+        if score >= 100:
+            finish = True
+            window.blit(txt_win_game, [200, 200])
+
+        if lost == 3:
+            finish = True
+            window.blit(txt_lose_game, [200, 200])
+
+        display.update() 
+    
+    else:
+        score = 0
+        lost = 0
+        finish = False
+        
+        time.delay(3000)
+        for i in range(1, 6): 
+
+
+    time.delay(50)
