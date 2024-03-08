24a06172dbc8ea73aa94e13eaca637e9f0858291
ItsMyp <146660335+ItsMyp@users.noreply.github.com>
2024-03-08 20:01:24 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pygame import *
+from random import randint 
 
 init()
 back = (200, 255, 255)
@@ -49,10 +50,17 @@ class Player(GameSprite):
             self.count = (self.count + 1) % len(player_l)  
             window.blit(player_r[self.count], (self.rect.x, self.rect.y))
         else:
-            self.count=0
+            self.count = (self.count + 1) % len(player_l)  
             window.blit(player_r[self.count], (self.rect.x, self.rect.y))
 
-
+class Enemy(GameSprite): 
+    def update(self): 
+        self.rect.y += self.speed 
+        global lost
+        if self.rect.y > win_height: 
+            self.rect.x = randint(80, win_width - 80) 
+            self.rect.y = 0 
+            lost = lost + 1
 
 
 
@@ -62,31 +70,32 @@ display.set_caption("WhirlyBird")
 
 clock = time.Clock()
 
-game= False
+game = False
 menu = True
 
 platform_img = 'platform.png' 
 spikes_img = 'spikes.png'
+
+score = 0
 
 font.init() 
 font3 = font.Font(None, 36)
 font2 = font.Font(None, 36) 
 font1 = font.Font(None,100)
 
-def game_menu():
-    pass
+platform = sprite.Group() 
+spike = sprite.Group() 
 
-def level_1():
-    pass
-
-def level_2():
-    pass
-
+jumping = False
 running = True
 player = Player('PlayerR1.png', 5, 300, 85, 100, 10)
 x_bg = 0
 y_bg=0
+jump_count=10
+jump_height=10
 while running:
+    keys = key.get_pressed()
+
     for e in event.get():
         if e.type == QUIT:
             running = False
@@ -104,17 +113,45 @@ while running:
         window.blit(bg, (y_bg+win_width, y_bg))
         y_bg=4
         
+        text = font2.render("Рахунок:" + str(score), 1, (255, 255, 255)) 
+        window.blit(text, (10, 20))
+
         player.update()
         player.animation()
 
+        platforms = transform.scale(image.load("platform.png"), (100, 10))
+        window.blit(platforms, (350, 300))
+
+        spikes = transform.scale(image.load("spikes.png"), (100, 33))
+        window.blit(spikes, (100, 300))
+
         if y_bg == win_width:
             y_bg=0
-    if menu:
-        text = font2.render("WhirlyBird", 1, (255, 255, 255))
-        window.blit(text, (325, 77))
+        elif not jumping:
+            if keys[K_SPACE]:
+                jumping = True
+        else:
+            if jump_count >= -jump_height:
+                neg = 1
+                if jump_count < 0:
+                    neg = -1
+                player.rect.y -= (jump_count ** 2) * 0.5 * neg
+                jump_count -= 1
+            else:
+                jumping = False
+                jump_count = jump_height
 
-        playerintro = Player('PlayerIntro.png', 5, 300, 85, 100, 10)
-        
+    if menu:
+        window.fill(back)
+        window.blit(bg, (y_bg, y_bg))
+        window.blit(bg, (y_bg+win_width, y_bg))
+        text = font2.render("WhirlyBird", 1, (0, 0, 0))
+        window.blit(text, (325, 77))
+        playerintro = transform.scale(image.load("PlayerIntro.png"), (80, 95))
+        window.blit(playerintro, (350, 300))
+    
+   
+
     display.update()
 
     time.delay(FPS)
