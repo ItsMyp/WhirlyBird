bf3b44c2e33f711d0180d1ed17865d3ad6d11ffc
ItsMyp <146660335+ItsMyp@users.noreply.github.com>
2024-03-27 20:40:30 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from random import randint
 
 init()
 back = (200, 255, 255)
-win_width = 800
-win_height = 600
+win_width = 600
+win_height = 800
 window = display.set_mode((win_width, win_height))
 player_l = [image.load('PlayerL1.png'), image.load('PlayerL2.png'), image.load('PlayerL3.png')]
 player_r = [image.load('PlayerR1.png'), image.load('PlayerR2.png'), image.load('PlayerR3.png')]
@@ -53,18 +53,20 @@ class Player(GameSprite):
             self.count = (self.count + 1) % len(player_l)  
             window.blit(player_r[self.count], (self.rect.x, self.rect.y))
 
-class Platformers(GameSprite): 
+
+
+class Platform(GameSprite):
     def update(self): 
         self.rect.y += self.speed 
-        global lost
         if self.rect.y > win_height: 
-            self.rect.x = randint(80, win_width - 80) 
+            self.rect.x = randint(120, win_width - 120) 
             self.rect.y = 0 
-            lost = lost + 1
+        
+            
 
+        
 
-
-FPS = 40
+FPS = 24
 bg = transform.scale(image.load("bg.png"), (win_width, win_height))
 display.set_caption("WhirlyBird")
 
@@ -83,22 +85,28 @@ font3 = font.Font(None, 36)
 font2 = font.Font(None, 36) 
 font1 = font.Font(None,100)
 
-platforms = sprite.Group()
-for i in range(1, 6): 
-    platform = Platformers(platform_img, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
-    platforms.add(platform)
 
-spikes = sprite.Group() 
 
-txt_lose_game = font1.render('YOU LOSE', True, [255, 0, 0])
-
-jumping = False
+jumping = True
 running = True
 player = Player('PlayerR1.png', 300, 300, 100, 100, 10)
 x_bg = 0
 y_bg=0
 jump_count=10
 jump_height=10
+
+
+platforms = sprite.Group()
+platforms.add(Platform(platform_img,140,140,120,20,20))#1
+platforms.add(Platform(platform_img,300, 300,120,20,20))#2
+platforms.add(Platform(platform_img,100,win_height - 150,120,20,20))#3
+
+spikes = sprite.Group()
+
+spikes.add(Platform(spikes_img,200, win_height,120,50,20))#4
+
+
+
 while running:
     keys = key.get_pressed()
 
@@ -117,32 +125,58 @@ while running:
         window.fill(back)
         window.blit(bg, (y_bg, y_bg))
         window.blit(bg, (y_bg+win_width, y_bg))
+        platforms.draw(window)
+        platforms.update()
+        spikes.draw(window)
+        spikes.update()
         y_bg=4
         
-        text = font2.render("Рахунок:" + str(score), 1, (255, 255, 255)) 
-        window.blit(text, (100, 200))
-
-    
-
+        text = font2.render("Рахунок:" + str(score), 1, (0, 0, 0)) 
+        window.blit(text, (10, 20))
         player.update()
         player.animation()
-
-        platforms = transform.scale(image.load("platform.png"), (100, 10))
-        window.blit(platforms, (300, 420))
-
-        spikes = transform.scale(image.load("spikes.png"), (100, 33))
-        window.blit(spikes, (100, 300))
+        
         
 #        if not sprite.spritecollide(player, platforms, True):
 #            player.rect.y -=3
-            
 
+        hits_lose = sprite.spritecollide(player,spikes,False)
+        if hits_lose:
+            text_lose = font2.render('YOU LOSE', True, [255, 0, 0])
+            window.blit(text_lose, (100, 200))
+            # game = False
+        
+        hits = sprite.spritecollide(player,platforms,False)
+        for platform in hits:
+            if player.rect.bottom> platform.rect.top:
+                    player.rect.bottom =  platform.rect.top
+                    player.vel_y =0 
+
+            elif player.rect.top < platform.rect.bottom:
+                    player.rect.top =  platform.rect.bottom-10
+                    player.vel_y =0 
+                    
+        
+        
+        # if hits:
+        #     player.rect.bottom = hits[0].rect.top-10
+            
+        #     score+=1
+        #     player.update()
+            
+            
+        if not hits:
+            player.rect.bottom +=5 
+        if player.rect.y > win_height:
+            text_lose = font2.render('YOU LOSE', True, [255, 0, 0])
+            window.blit(text_lose, (100, 200))
+            
 
         if y_bg == win_width:
             y_bg=0
         elif not jumping:
             if keys[K_SPACE]:
-                jumping = True
+                jumping = False
         else:
             if jump_count >= -jump_height:
                 neg = 1
@@ -151,7 +185,7 @@ while running:
                 player.rect.y -= (jump_count ** 2) * 0.5 * neg
                 jump_count -= 1
             else:
-                jumping = False
+                jumping = True
                 jump_count = jump_height
 
     if menu:
@@ -159,9 +193,9 @@ while running:
         window.blit(bg, (y_bg, y_bg))
         window.blit(bg, (y_bg+win_width, y_bg))
         text = font2.render("WhirlyBird", 1, (0, 0, 0))
-        window.blit(text, (325, 77))
+        window.blit(text, (235, 250))
         playerintro = transform.scale(image.load("PlayerIntro.png"), (80, 95))
-        window.blit(playerintro, (350, 300))
+        window.blit(playerintro, (260, 350))
 
     display.update()
 
