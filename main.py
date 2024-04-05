7027ac7491caae8a96267dc80190e68875821a57
ItsMyp <146660335+ItsMyp@users.noreply.github.com>
2024-04-05 16:09:19 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from pygame import *
 from random import randint 
-
+import sys
 init()
 back = (200, 255, 255)
 win_width = 600
-win_height = 800
+win_height = 600
 window = display.set_mode((win_width, win_height))
 player_l = [image.load('PlayerL1.png'), image.load('PlayerL2.png'), image.load('PlayerL3.png')]
 player_r = [image.load('PlayerR1.png'), image.load('PlayerR2.png'), image.load('PlayerR3.png')]
+
+clock = time.Clock()
+start_time = time.get_ticks()
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -52,12 +55,21 @@ class Player(GameSprite):
         else:
             self.count = (self.count + 1) % len(player_l)  
             window.blit(player_r[self.count], (self.rect.x, self.rect.y))
+    def check_collisions_down(self, blocks):
+        is_on_ground = False
+        for block in blocks:
+            if self.rect.colliderect(block.rect):
+                if self.rect.y <= block.rect.y-40:
+                    self.rect.bottom = block.rect.y + 7.5
+                    is_on_ground = True
+        if not is_on_ground:
+             self.rect.y += 12
 
 
 
 class Platform(GameSprite):
     def update(self): 
-        self.rect.y += self.speed 
+        self.rect.y += 7 
         if self.rect.y > win_height: 
             self.rect.x = randint(120, win_width - 120) 
             self.rect.y = 0 
@@ -88,35 +100,36 @@ font1 = font.Font(None,100)
 
 
 
-jumping =   True
 running = True
-player = Player('PlayerR1.png', 235, 700, 100, 100, 10)
+player = Player('PlayerR1.png', 235, 300, 100, 100, 10)
 x_bg = 0
 y_bg=0
-jump_count=10
-jump_height=10
-
+jump_count = 7.5
+jump_height = 7.5
+jumping = False
+text_score = font2.render(f'Рахунок:{score}',True,(255,255,22))
 
 platforms = sprite.Group()
-platforms.add(Platform(platform_img,400,win_height - 0,120,20,5))#1
-platforms.add(Platform(platform_img,300,win_height - 200,120,20,5))#2
-platforms.add(Platform(platform_img,200,win_height - 400,120,20,5))#3
-platforms.add(Platform(platform_img,100,win_height - 600,120,20,5))#4
+platforms.add(Platform(platform_img,100,win_height - 0,120,20,5))#1
+platforms.add(Platform(platform_img,200,win_height - 200,120,20,5))#2
+platforms.add(Platform(platform_img,300,win_height - 400,120,20,5))#3
+platforms.add(Platform(platform_img,500,500,120,20,5))#1
 
 
 
 while running:
     keys = key.get_pressed()
+    current_time = time.get_ticks()
+    game_time = (current_time - start_time)/1000
 
     for e in event.get():
         if e.type == QUIT:
             running = False
-        elif  e.type==KEYDOWN :
-            game = True
-            menu = False
-        elif e.type == K_SPACE:
-            game = False
-            menu = True
+        if  e.type==KEYDOWN :
+            if e.key == K_UP and menu == True:
+                game = True
+                menu = False
+
         
     
 
@@ -126,46 +139,19 @@ while running:
         window.blit(bg, (y_bg+win_width, y_bg))
         platforms.draw(window)
         platforms.update()
-        #spikes.draw(window)
-        #spikes.update()
+        text_score = font2.render(f'Таймер:{game_time}',True,(0,255,0))
+        window.blit(text_score, (20, 20))
+
+
         y_bg=4
-        
-#        text = font2.render("Рахунок:" + str(score), 1, (0, 0, 0)) 
-#        window.blit(text, (10, 20))
+ 
         player.update()
         player.animation()
+        player.check_collisions_down(platforms)
+    
         
         
-#        if not sprite.spritecollide(player, platforms, True):
-#            player.rect.y -=3
 
-#        hits_lose = sprite.spritecollide(player,spikes,False)
-#        if hits_lose:
-#            text_lose = font2.render('YOU LOSE', True, [255, 0, 0])
-#            window.blit(text_lose, (350, 250))
-#            game = False
-        
-        hits = sprite.spritecollide(player,platforms,False)
-        for platform in hits:
-            if player.rect.bottom> platform.rect.top:
-                    player.rect.bottom =  platform.rect.top
-                    player.vel_y =0 
-
-            elif player.rect.top < platform.rect.bottom:
-                    player.rect.top =  platform.rect.bottom-10
-                    player.vel_y =0 
-                    
-        
-        
-        # if hits:
-        #     player.rect.bottom = hits[0].rect.top-10
-            
-        #     score+=1
-        #     player.update()
-            
-            
-        if not hits:
-            player.rect.bottom +=5 
         if player.rect.y > win_height:
             text_lose = font2.render('YOU LOSE', True, [255, 0, 0])
             window.blit(text_lose, (235, 250))
@@ -175,19 +161,23 @@ while running:
 
         if y_bg == win_width:
             y_bg=0
-        elif not jumping:
-            if keys[K_SPACE]:
-                jumping = False
+        if not jumping:
+            if keys[K_UP]:
+                jumping = True 
+
+
         else:
             if jump_count >= -jump_height:
                 neg = 1
                 if jump_count < 0:
                     neg = -1
-                player.rect.y -= (jump_count ** 2) * 0.5 * neg
-                jump_count -= 1
+
+                player.rect.y -= (jump_count ** 2)  * neg
+                jump_count -= 0.8
             else:
-                jumping = True
+                jumping = False
                 jump_count = jump_height
+
 
     if menu:
         window.fill(back)
